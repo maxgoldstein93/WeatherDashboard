642ba77b977149d08302b609db2391548015b929
Max Goldstein <magoldstein93@gmail.com>
2020-09-15 10:05:22 -0400

    working UV index color

--- weather.js
+++ weather.js
@@ -3,22 +3,32 @@ $(document).ready(function () {
     $("#searchCity").on("click", function (event) {
         event.preventDefault();
 
+
         var city = $("#userCity").val();
+        // localStorage.setItem($("#userCity", city));
+        localStorage.setItem($(this).find("input").attr("id"), (city));
+        console.log(city)
+        
+
         var apiKey = "6ce841efb6b07e3f98309460d1f79c3c";
 
 
-    function renderPastCity(){
-        for(var i =0; i < city.length; i++)
-        var cityLi = $("<li>")
-        cityLi.text(city)
-        $("#cityList").append(cityLi);
-        // set local storage?
-        // add functionality to city name
-    }
+        function renderPastCity() {
+            for (var i = 0; i < city.length; i++)
+                var cityLi = $("<li>").addClass("list-group-item p-3")
+            cityLi.text(city)
+            $("#cityList").prepend(cityLi);
+
+            // add functionality to city name
+        }
+        var displayCity = localStorage.getItem("userCity");
+        var displayUserCity = document.getElementById("userCity");
+        displayUserCity.value = displayCity;
+        console.log(displayUserCity)
+        console.log(displayCity)
         renderPastCity();
 
 
-       
         // Get longitude and latitude
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey,
@@ -27,34 +37,34 @@ $(document).ready(function () {
             var lat = weatherData.coord.lat;
             var lon = weatherData.coord.lon;
 
-   
+
             // connect to api with declrared long and lat to pull date for city
 
             $.ajax({
                 url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly&appid=" + apiKey,
                 method: "GET"
             }).then(function (response) {
-                
+
                 console.log(response)
                 var currentDate = response.current.dt;
-                console.log(currentDate);
+
                 var todaysDate = "";
                 var humidity = response.current.humidity;
-                console.log(humidity);
+
                 var windSpeed = response.current.wind_speed;
-                console.log(windSpeed)
+
                 var uvIndex = response.current.uvi;
-                console.log(uvIndex)
+
                 var icon = response.current.weather[0].icon;
-                console.log(icon)
+
                 var temp = response.current.temp
-                console.log(temp)
-                    // IMG in main
+
+                // IMG in main
                 var iconLink = "http://openweathermap.org/img/wn/" + icon + ".png"
                 var newImg = $("#icon")
                 newImg.attr("src", iconLink)
                 newImg.append(iconLink)
-                
+
 
                 // Covert temp to farenhite
                 function convert() {
@@ -80,17 +90,18 @@ $(document).ready(function () {
                 convert();
 
                 var tempF = ((temp - 273.15) * 1.80 + 32).toFixed(2);
-                    // display on screen
+                // display on screen
                 $("#cityChoice").text(city)
                 $("#date").text(todaysDate)
                 $("#temperature").text("Current Temperature: " + tempF + "°F")
                 $("#humidity").text("Humidity: " + humidity + "%")
                 $("#windSpeed").text("Wind Speed: " + windSpeed + "MPH")
-                $("#uvIndex").text("UV Index " + uvIndex)
+                $("#colorIndex").text(uvIndex)
 
                 // fill cards with 5 day forcast
 
                 for (var i = 1; i < 6; i++) {
+
                     var fiveDayDate = response.daily[i].dt;
                     function convertFiveDayDate() {
                         // Unixtimestamp https://makitweb.com/convert-unix-timestamp-to-date-time-with-javascript/
@@ -110,16 +121,12 @@ $(document).ready(function () {
                         console.log(read5DayDate);
                         return read5DayDate;
                     }
-                    var read5DayDate = convertFiveDayDate(); 
+                    var read5DayDate = convertFiveDayDate();
                     var fiveDayIcon = response.daily[i].weather[0].icon;
-                    console.log(fiveDayIcon);
                     var fiveDayTemp = response.daily[i].temp.day;
                     var fiveDayTempF = ((fiveDayTemp - 273.15) * 1.80 + 32).toFixed(2);
-                    console.log(fiveDayTempF);
                     var fiveDayHumidity = response.daily[i].humidity;
-                    console.log(fiveDayHumidity);
 
-                    
 
                     var card = $("<div>").addClass("card-img col-2 card card text-white bg-dark m-3");
                     // fill it with data[i]
@@ -137,6 +144,30 @@ $(document).ready(function () {
                     // append that card to #5day
                     $("#forecast").append(card)
                 }
+
+                // function to change UV Index backround color
+                function colorChange() {
+                    $("#colorIndex").each(function () {
+                        if (uvIndex < 4) {
+                            $("#colorIndex").addClass("green");
+                            $("#colorIndex").removeClass("red");
+                            $("#colorIndex").removeClass("yellow");
+                        } else if (uvIndex > 4 && uvIndex < 9) {
+                            $("#colorIndex").addClass("yellow");
+                            $("#colorIndex").removeClass("red");
+                            $("#colorIndex").removeClass("green");
+
+                        } else {
+                            $("#colorIndex").addClass("red")
+                            $("#colorIndex").removeClass("yellow");
+                            $("#colorIndex").removeClass("green");
+                        }
+                    });
+                }
+
+                colorChange();
+
+                
             });
         });
     });
